3b11528da95fc1db2c04d95040b6dcece4f64b3d
MixalisP2025 <mpervolianakis@ais.gr>
2026-02-02 14:03:10 +0200

    Switch to Clerk hosted authentication pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ClerkProvider } from "@clerk/clerk-react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { ClerkProvider, SignedIn, SignedOut, RedirectToSignIn } from "@clerk/clerk-react";
 import App from "./App.jsx";
-import SignInPage from "./pages/SignIn.jsx";
-import SignUpPage from "./pages/SignUp.jsx";
-import ProtectedRoute from "./components/ProtectedRoute.jsx";
 import "./index.css";
 
 const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
@@ -17,20 +13,12 @@ if (!clerkPubKey) {
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ClerkProvider publishableKey={clerkPubKey}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/sign-in/*" element={<SignInPage />} />
-          <Route path="/sign-up/*" element={<SignUpPage />} />
-          <Route
-            path="/*"
-            element={
-              <ProtectedRoute>
-                <App />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
-      </BrowserRouter>
+      <SignedIn>
+        <App />
+      </SignedIn>
+      <SignedOut>
+        <RedirectToSignIn />
+      </SignedOut>
     </ClerkProvider>
   </React.StrictMode>
 );
